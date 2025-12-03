c0f55f68f295b8db7a6695e3f620aa843701f89a
Kenn-altsam <a.kenzhe.sam@gmail.com>
2025-12-03 20:32:45 +0500

    prim for getting only weights

---
+++ prim_kruskal/prim2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+typedef pair<int, int> PII;
+
+int prim(int n, vector<vector<PII>> &adj, int start){
+    priority_queue<PII, vector<PII>, greater<PII>> pq;
+    vector<int> visited(n, false);
+
+    pq.push({0, start}); // weight, node
+    int sum = 0;
+
+    while(!pq.empty()){
+        PII it = pq.top();
+        pq.pop();
+        int weight = it.first;
+        int node = it.second;
+
+        if(visited[node] == true){
+            continue;
+        }
+
+        visited[node] = true;
+        sum += weight;
+
+        for(PII it : adj[node]){
+            int adjNode = it.first;
+            int adjWeight = it.second;
+
+            if(!visited[adjNode]){
+                pq.push({adjWeight, adjNode});
+            }
+        }
+    }
+
+    return sum;
+}
+
+int main(){
+
+    vector<vector<PII>> adj(4);
+
+    adj[0].push_back({1, 13});
+    adj[0].push_back({2, 12});
+
+    adj[1].push_back({0, 13});
+    adj[1].push_back({2, 3});
+    adj[1].push_back({3, 14});
+
+    adj[2].push_back({0, 12});
+    adj[2].push_back({1, 3});
+    adj[2].push_back({3, 9});
+
+    adj[3].push_back({1, 14});
+    adj[3].push_back({2, 9});
+
+    cout << prim(4, adj, 0);
+
+    return 0;
+}
